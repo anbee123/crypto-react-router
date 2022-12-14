1bfe902102ed9746e9069ba79ac4778e68cfe3c8
An Bui <anhipchipchip@gmail.com>
2022-12-13 22:58:10 -0500

    make an API call

--- src/pages/Currencies.jsx
+++ src/pages/Currencies.jsx
@@ -1,6 +1,27 @@
-const Currencies = () => {
-    return (
-    <h1>Currencies component</h1> )
+import { Link } from "react-router-dom"
 
+const Currencies = (props) => {
+  const currencies = [
+    { name: "Bitcoin", symbol: "BTCUSD" },
+    { name: "Litecoin", symbol: "LTCUSD" },
+    { name: "Ethereum", symbol: "ETHUSD" },
+    { name: "Zcash", symbol: "ZECUSD" },
+    { name: "Bitcoin Cash", symbol: "BCHUSD" },
+  ]
+
+  return (
+    <div className="currencies">
+      {currencies.map((coin) => {
+        const { name, symbol } = coin
+
+        return (
+          <Link to={`/price/${symbol}`}>
+            <h2>{name}</h2>
+          </Link>
+        )
+      })}
+    </div>
+  )
 }
+
 export default Currencies
--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import (useState)
 const Price = () => {
-    return(
 
- <h1>Price component</h1> 
+    const apiKey = 'pk_4c80d13459b74d8b979a15d454bd6926'
+    // Grabbing the Currency symbol from the URL Params
+  const params = useParams()
+  const symbol = params.symbol
+  // Using the other two variables to create our URL
+  const url = `https://cloud.iexapis.com/stable/crypto/${symbol}/price?token=${apiKey}`
+
+  //state to hold the coin data
+  const [coin, setCoin] = useState("null")
+
+  //function to fetch coin data
+  const getCoin = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setCoin(data)
+  }
+
+  // useEffect to run getCoin when component mounts
+  useEffect(() => {
+    getCoin()
+  }, [])
+
+  // loaded function for when data is fetched
+  const loaded = () => {
+    return (
+      <div>
+        <h1>{symbol}</h1>
+        <h2>{coin.price}</h2>
+      </div>
     )
+  }
+
+  // Function for when data doesn't exist
+  const loading = () => {
+    return <h1>Loading...</h1>
+  }
+
+  // if coin has data, run the loaded function, otherwise, run loading
+  return coin ? loaded() : loading()
 }
+
 export default Price
