61325fb2751efc38a2429b496fbff4d2711148a0
An Bui <anhipchipchip@gmail.com>
2022-12-13 21:57:21 -0500

    import route, routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Main from './pages/Main';
 import Price from './pages/Price';
 import Currencies from './pages/Currencies';
+import {Route, Routes} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Main />
-     <Price />
-     <Currencies />
-    
+<Routes>
+  <Route path='/' element={<Main />} />
+  <Route path='/currencies' element={<Currencies />}/>
+  <Route path='/price' element={<Price />} />
+</Routes>
+
+  
     </div>
   );
 }
